d60f97452a4d4ddd3ea39945214b185927226685
Umesh-malik-byjus <umesh.malik@byjus.com>
2022-02-27 18:18:30 +0530

    adding isr

---
+++ pages/isr/index.js
@@ -0,0 +1,41 @@
+import Link from "next/link";
+
+const Index = (props = {}) => {
+    const { post = [] }= props;
+    return (
+        <div>
+            <div style={{float:'right', marginRight:'2vw'}}>
+                <Link href='/add-post' passHref>
+                    <button>Add New Post</button>
+                </Link>
+            </div>
+            <h2>Cached Posts</h2>
+            <div>
+                {post && post.map(p => (
+                    <div key={p.id}>
+                        <Link href={`/isr/${p.id}`} passHref>
+                            <h3 style={{cursor:"pointer", textDecoration:'underline'}}>{p.title}</h3>
+                        </Link>
+                        <p>{p.body}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Index
+
+export async function getStaticProps() {
+    const url = `${process.env.HOST_URL}/api/get-posts`
+    const res = await fetch(url,{
+        method: 'GET'
+    });
+    const posts = await res.json();
+    return {
+        props: {
+            post: posts
+        },
+        revalidate: 10
+    }
+}
