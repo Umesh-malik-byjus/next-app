7a827a0ca250ad5253f1c40b2cc2d8ec59a40ac0
Umesh-malik-byjus <umesh.malik@byjus.com>
2022-02-27 13:57:11 +0530

    dfgdg

--- pages/static-page/index.js
+++ pages/static-page/index.js
@@ -20,16 +20,11 @@ const index = (props) => {
 export default index;
 
 export async function getStaticProps() {
-    console.log('getStaticProps');
-    console.log(process.env.HOST_URL);
     const url = `${process.env.HOST_URL}/api/get-post`
-    console.log(url);
     const res = await fetch(url,{
         method: 'GET'
     });
-    console.log(res)
     const posts = await res.json();
-    console.log(posts);
     return {
         props: {
             post: posts
