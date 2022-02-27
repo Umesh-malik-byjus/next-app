05ad779d33db169e51e36647ef9a2bb8a8593339
Umesh-malik-byjus <umesh.malik@byjus.com>
2022-02-27 13:46:21 +0530

    ....

---
+++ pages/static-page/index.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+
+const index = (props) => {
+    const { post = [] }= props;
+    return (
+        <div>
+            <h2>Posts</h2>
+            <div>
+                {post.map(p => (
+                    <div key={p.id}>
+                        <h3>{p.title}</h3>
+                        <p>{p.body}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default index;
+
+// export async function getStaticProps() {
+//     console.log('getStaticProps');
+//     console.log(process.env.HOST_URL);
+//     const res = await fetch(`${process.env.HOST_URL}/api/get-post`,{
+//         method: 'GET'
+//     });
+//     const posts = await res.json();
+//     return {
+//         props: {
+//             post: posts
+//         }
+//     }
+// }
